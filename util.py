8a6901d794d05a465dd64339b8865fa7ff025a43
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2011-04-01 18:20:28 +0200

    single quotes

--- util.py
+++ util.py
@@ -7,7 +7,7 @@ def relativePath(path):
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
 
 def addDatetime(quote):
-    quote['datetime'] = datetime.datetime.strptime(quote['date'], "%Y-%m-%d %H:%M:%S")
+    quote['datetime'] = datetime.datetime.strptime(quote['date'], '%Y-%m-%d %H:%M:%S')
     return quote
 
 def addPrettyDate(quote):
