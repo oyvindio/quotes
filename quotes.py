d267196799fbb8b73f9ab79c7a29530e31d7e85b
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2011-04-01 23:43:15 +0200

    Add optional google analytics snippet

--- quotes.py
+++ quotes.py
@@ -1,7 +1,7 @@
 import web
 from web.contrib.template import render_jinja
 from util import fixDates
-from settings import DB, TEMPLATES, CHANNEL
+from settings import DB, TEMPLATES, CHANNEL, GOOGLE_ANALYTICS
 
 urls = (
     '/.*', 'Quotes',
@@ -14,7 +14,7 @@ class Quotes:
         # sort by added date, ascending
         quotes = sorted([fixDates(quote) for quote in quotes], cmp=lambda this, other: cmp(this['datetime'], other['datetime']), reverse=True)
         render = render_jinja(TEMPLATES)
-        return render.list(quotes=quotes, channel=CHANNEL)
+        return render.list(quotes=quotes, channel=CHANNEL, analytics=GOOGLE_ANALYTICS)
 
 if __name__ == '__main__':
     application = web.application(urls, globals())
