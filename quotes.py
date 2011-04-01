15134bcaf6cbc9861ec592538baa1f34145da0b3
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2011-04-01 16:10:29 +0200

    make channel configurable

--- quotes.py
+++ quotes.py
@@ -1,7 +1,7 @@
 import web
 from web.contrib.template import render_jinja
 from util import addPrettyDate
-from settings import DB, TEMPLATES
+from settings import DB, TEMPLATES, CHANNEL
 
 urls = (
     '/.*', 'Quotes',
@@ -10,11 +10,11 @@ urls = (
 class Quotes:
     def GET(self):
         db = web.database(dbn='sqlite', db=DB)
-        quotes = db.select('quote', where="channel = '#grouphugs'")
+        quotes = db.select('quote', where="channel = '{}'".format(CHANNEL))
         # sort by added date, ascending
         quotes = sorted([addPrettyDate(quote) for quote in quotes], cmp=lambda this, other: cmp(this['datetime'], other['datetime']), reverse=True)
         render = render_jinja(TEMPLATES)
-        return render.list(quotes=quotes)
+        return render.list(quotes=quotes, channel=CHANNEL)
 
 if __name__ == '__main__':
     application = web.application(urls, globals())
