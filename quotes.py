a1fe1dd53a2dd7f60282dc5e60061582a587cc59
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2011-08-08 20:02:37 +0200

    Implement pagination

--- quotes.py
+++ quotes.py
@@ -1,7 +1,9 @@
+from __future__ import division
+import math
 import web
 from web.contrib.template import render_jinja
 from util import fixDates
-from settings import DB, TEMPLATES, CHANNEL, GOOGLE_ANALYTICS
+from settings import DB, TEMPLATES, CHANNEL, GOOGLE_ANALYTICS, PER_PAGE
 
 urls = (
     '/.*', 'Quotes',
@@ -9,12 +11,21 @@ urls = (
 
 class Quotes(object):
     def GET(self):
+        params = web.input()
+        page = int(params.p) if hasattr(params, 'p') else 1
+        offset = (page - 1) * PER_PAGE
         db = web.database(dbn='sqlite', db=DB)
-        quotes = db.select('quote', where="channel = '{}'".format(CHANNEL), order='date desc')
+        quote_count = db.query("select count(*) as count from quote where channel='{}'".format(CHANNEL))[0].count
+        quotes = db.select('quote', where="channel = '{}'".format(CHANNEL),
+                           order='date desc', limit='$l', offset='$o',
+                           vars={'l': PER_PAGE, 'o': offset})
         quotes = [fixDates(quote) for quote in quotes]
+        pages = int(math.ceil(quote_count / PER_PAGE))
         render = render_jinja(TEMPLATES)
-        return render.list(quotes=quotes, channel=CHANNEL, analytics=GOOGLE_ANALYTICS)
+        return render.list(quotes=quotes, channel=CHANNEL,
+                           analytics=GOOGLE_ANALYTICS, page=page, pages=pages)
 
 if __name__ == '__main__':
     application = web.application(urls, globals())
     application.run()
+
